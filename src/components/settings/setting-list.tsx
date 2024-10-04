a390603d6f5a8ce1149c47cc13e8b81ffee8ed17
leesuyeon <lgh0385hh@gmail.com>
2024-10-04 17:35:54 +0900

    FEAT: 사용자 전환 모달 생성

--- src/components/settings/setting-list.tsx
+++ src/components/settings/setting-list.tsx
@@ -12,9 +12,10 @@ export default function SettingList() {
     ];
 
     return (
-        <div className="w-full flex flex-col justify-start items-start " >
+        <div className="w-full flex flex-col justify-start items-start gap-2 ml-2" >
             {settingList.map(({ listName, href, Icon }) => (
-                <Link href={href} key={listName} className="cursor-pointer hover:text-blue flex items-center py-1 justify-center gap-3 text-lg text-gray-700 text-md">
+                <Link href={href} key={listName} className="cursor-pointer hover:text-blue hover:font-semibold flex items-center py-1 justify-center gap-3 text-lg
+                text-gray-700 text-md transition-trasition duration-300">
                     <Icon className='w-5 h-5' />
                     <div>{listName}</div>
                 </Link>
