e1957dc383e843580df16d08219907caab048ad6
이현지 <dororong555@gmail.com>
2024-10-05 17:56:00 +0900

    의존성 충돌 해결

--- src/app/manual/page.tsx
+++ src/app/manual/page.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import ManualLayout from './layout';
-import Image from 'next/image'; // Image 컴포넌트 import
-import image1 from './images/image1.png'; // 첫 번째 이미지 경로
-import image2 from './images/image2.png'; // 두 번째 이미지 경로
+import Image from 'next/image';
+import image1 from './images/image1.png';
+import image2 from './images/image2.png';
 
 const ManualPage: React.FC = () => {
   return (
-    <ManualLayout>
-      <div className="min-w-60 w-full m-5 p-5 bg-white rounded-lg shadow-lg text-center text-[#136BFF] text-2xl font-bold">
-        설명
+    <section className="mx-5 py-5">
+      <div className="min-w-60 w-full p-5 bg-white rounded-lg shadow-lg text-center text-[#136BFF] text-2xl font-bold mb-5">
+        설명서
       </div>
 
-      <div className="flex justify-between mx-5 mt-5 overflow-hidden"> 
-        <div className="flex flex-col items-center mr-10">
+      <div className="flex flex-col sm:flex-row justify-between overflow-hidden">
+        {/* 카카오톡 섹션 */}
+        <div className="flex flex-col items-center mb-5 sm:mb-0 sm:mr-10">
           <div className="w-36 h-36 rounded-full bg-gray-300 flex items-center justify-center">
             <Image 
               src={image1} 
               alt="Image 1" 
               className="rounded-full object-cover" 
-              width={144} // 너비 설정
-              height={144} // 높이 설정
+              width={144} 
+              height={144} 
             />
           </div>
           <p className="mt-2 text-2xl text-[#136BFF] font-bold">카카오톡</p>
@@ -31,14 +31,15 @@ const ManualPage: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex flex-col items-center ml-10">
+        {/* 일상 섹션 */}
+        <div className="flex flex-col items-center">
           <div className="w-36 h-36 rounded-full bg-gray-300 flex items-center justify-center">
             <Image 
               src={image2} 
               alt="Image 2" 
               className="rounded-full object-cover" 
-              width={144} // 너비 설정
-              height={144} // 높이 설정
+              width={144} 
+              height={144} 
             />
           </div>
           <p className="mt-2 text-2xl text-[#136BFF] font-bold">일상</p>
@@ -56,7 +57,7 @@ const ManualPage: React.FC = () => {
           </div>
         </div>
       </div>
-    </ManualLayout>
+    </section>
   );
 };
 
