4e5e17392ab449ee836bad9ce56735002fb220d6
Sonjieun2 <jeun2e2@gmail.com>
2024-10-08 02:28:49 +0900

    FEAT: 회원가입 제작

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,9 +2,9 @@ import Guardian from '../../../components/register/guardian';
 
 export default function signup() {
   return (
-    <div className="flex flex-col justify-center items-center mt-[80px]">
-      <div className="flex flex-col gap-7 sm:gap-5 px-4 sm:px-20 h-[650px] sm:h-[500px] w-[350px] sm:w-[700px] justify-center items-center bg-white font-bold rounded-[40px] border-2 border-blue">
-        <span className="my-5 text-3xl">회원가입</span>
+    <div className="flex flex-col justify-center items-center mt-[50px]">
+      <div className="flex flex-col gap-7 sm:gap-5 px-4 sm:px-20 h-[650px] sm:h-[550px] w-[350px] sm:w-[850px] justify-center items-center bg-white font-bold rounded-[40px] border-4 border-blue">
+        <span className="text-3xl">회원가입</span>
         <div className="flex flex-row gap-10">
           <label className="flex items-center gap-2">
             <input
--- src/components/register/guardian.tsx
+++ src/components/register/guardian.tsx
@@ -1,12 +1,52 @@
+"use client";
+
+import React from 'react';
+
+interface InputProps {
+  label: string;
+  type: string;
+  children?: React.ReactNode;
+  onInput?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function formatPhoneNumber(value: string) {
+  return value
+    .replace(/[^0-9]/g, '')
+    .replace(/(^02.{0}|^01.{1}|[0-9]{3,4})([0-9]{3,4})([0-9]{4})/, "$1-$2-$3");
+}
+
 export default function GuardianRegister() {
-  return (
-    <div className="flex flex-row text-md sm:text-lg items-center justify-between w-full gap-4">
-      <span className="hidden sm:block flex-[2]">아이디</span>
+  const handlePhoneInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target;
+    target.value = formatPhoneNumber(target.value);
+  };
+
+  const Input = ({ label, children, type, onInput }: InputProps) => (
+    <div className="flex flex-row gap-3 items-center justify-start w-full gap-4 ml-[100px]">
+      <span className="w-[120px]">{label}</span>
       <input 
-        type="text"
-        className="flex-[8] border-2 border-darkblue rounded-lg p-1"
+        type={type}
+        className="w-[300px] border-2 border-gray-400 rounded-xl p-1"
+        onInput={onInput}
       />
-      <span className="bg-blue text-white rounded-xl p-2 mx-1">중복 확인</span>
+      {children && (
+        <div>{children}</div>
+      )}
+    </div>
+  );
+
+  return (
+    <div className="flex flex-col sm:text-lg items-center justify-center w-full gap-4">
+      <Input label="아이디" type="text">
+        <span className="w-[100px] bg-slate-600 text-white rounded-md p-2 mx-1 text-center">중복 확인</span>
+      </Input>
+      <Input label="비밀번호" type="password" />
+      <Input label="비밀번호 확인" type="password" />
+      <Input label="이름" type="text" />
+      <Input label="전화번호" type="text" onInput={handlePhoneInput}>
+        <span className="w-[100px] bg-slate-600 text-white rounded-md p-2 mx-1 text-center">인증번호</span>
+      </Input>
+      <Input label="인증번호 확인" type="text" />
     </div>
   );
 }
