63c6ee6e7443bd52764f3c6846542a6815720046
Sonjieun2 <jeun2e2@gmail.com>
2024-10-08 03:23:36 +0900

    FEAT: 회원가입 반응형 추가

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,8 +2,8 @@ import Guardian from '../../../components/register/guardian';
 
 export default function signup() {
   return (
-    <div className="flex flex-col justify-center items-center mt-[50px]">
-      <div className="flex flex-col gap-7 sm:gap-5 px-4 sm:px-20 h-[650px] sm:h-[550px] w-[350px] sm:w-[850px] justify-center items-center bg-white font-bold rounded-[40px] border-4 border-blue">
+    <div className="flex flex-col justify-center items-center mt-[30px]">
+      <div className="flex flex-col gap-2 sm:gap-5 px-4 md:px-20 h-[750px] md:h-[600px] w-[380px] sm:w-[600px] md:w-[850px] justify-center items-center bg-white font-bold rounded-[40px] border-4 border-blue">
         <span className="text-3xl">회원가입</span>
         <div className="flex flex-row gap-10">
           <label className="flex items-center gap-2">
--- src/components/register/guardian.tsx
+++ src/components/register/guardian.tsx
@@ -22,31 +22,39 @@ export default function GuardianRegister() {
   };
 
   const Input = ({ label, children, type, onInput }: InputProps) => (
-    <div className="flex flex-row gap-3 items-center justify-start w-full gap-4 ml-[100px]">
-      <span className="w-[120px]">{label}</span>
-      <input 
-        type={type}
-        className="w-[300px] border-2 border-gray-400 rounded-xl p-1"
-        onInput={onInput}
-      />
-      {children && (
-        <div>{children}</div>
-      )}
+    <div className="flex flex-col md:flex-row sm:gap-3 sm:items-center justify-start w-full gap-4 md:ml-[100px] mt-2">
+      <span className="w-full md:w-[120px]">{label}</span>
+      <div className="flex flex-row w-full gap-2">
+        <input 
+          type={type}
+          className="w-[230px] md:w-[300px] border-2 border-gray-400 rounded-xl p-1"
+          onInput={onInput}
+        />
+        {children && (
+          <div>{children}</div>
+        )}
+      </div>
     </div>
   );
 
   return (
-    <div className="flex flex-col sm:text-lg items-center justify-center w-full gap-4">
-      <Input label="아이디" type="text">
-        <span className="w-[100px] bg-slate-600 text-white rounded-md p-2 mx-1 text-center">중복 확인</span>
-      </Input>
-      <Input label="비밀번호" type="password" />
-      <Input label="비밀번호 확인" type="password" />
-      <Input label="이름" type="text" />
-      <Input label="전화번호" type="text" onInput={handlePhoneInput}>
-        <span className="w-[100px] bg-slate-600 text-white rounded-md p-2 mx-1 text-center">인증번호</span>
-      </Input>
-      <Input label="인증번호 확인" type="text" />
+    <div>
+      <div className="flex flex-col md:text-lg items-center justify-center w-full gap-1 md:gap-4 max-h-screen overflow-y-auto">
+        <Input label="아이디" type="text">
+          <span className="w-[100px] bg-slate-600 text-white rounded-md p-2 mx-1 text-center">중복 확인</span>
+        </Input>
+        <Input label="비밀번호" type="password" />
+        <Input label="비밀번호 확인" type="password" />
+        <Input label="이름" type="text" />
+        <Input label="전화번호" type="text" onInput={handlePhoneInput}>
+          <span className="w-[100px] bg-slate-600 text-white rounded-md p-2 mx-1 text-center">인증번호</span>
+        </Input>
+        <Input label="인증번호 확인" type="text" />
+      </div>
+      <div className="flex sm:flex-row">
+        <span className="flex justify-center items-center bg-darkblue text-white text-xl mt-[40px] p-2 rounded-xl w-full m-5">취소</span>
+        <span className="flex justify-center items-center bg-darkblue text-white text-xl mt-[40px] p-2 rounded-xl w-full m-5">가입</span>
+      </div>
     </div>
   );
 }
