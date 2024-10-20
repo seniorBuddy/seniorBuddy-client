e3a6f596fedaba56cf8e0972ca6838aa39a88089
leesuyeon <lgh0385hh@gmail.com>
2024-10-20 16:15:06 +0900

    FEAT: 메인 META 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { ThemeProviders } from "@/components/theme-providers";
 import { cookies } from "next/headers";
+import Head from "next/head";  // Head 컴포넌트를 import
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
@@ -22,6 +23,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Senior Buddy</title>  {/* 필요에 따라 title 태그도 추가 가능 */}
+      </Head>
       <body className={pretendard.variable}>
         <ThemeProviders initTheme={theme}>
           <main className="font-pretendard min-h-screen dark:bg-slate-800 text-white dark:text-slate-800">
