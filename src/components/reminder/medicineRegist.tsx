296cc4418a2109cd230d09aeaacc033d1b89e31f
Sonjieun2 <jeun2e2@gmail.com>
2024-10-23 03:55:02 +0900

    FEAT: 약 등록 UI 추가

--- src/components/reminder/medicineRegist.tsx
+++ src/components/reminder/medicineRegist.tsx
@@ -4,56 +4,95 @@ import Sample from '../../app/assets/images/sample.png';
 
 export default function medicineRegister({prop}:{prop: boolean}) {
   const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(e.target.checked);
   }
 
+  const handleHourSetting = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHours(e.target.value);
+  };
+
+  const handleMinuteSetting = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMinutes(e.target.value);
+  }
+
+  const handleSetAlarm = () => {
+    const alarmTime = `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}`;
+  }
+
+  const time = ['아침', '점심', '저녁'];
+  const date = ['월', '화', '수', '목', '금', '토', '일'];
+
   return (
-    <div className="w-[650px] h-[500px] bg-white border-4 rounded-2xl border-darkblue">
-      <div className="flex flex-row justify-center items-center gap-4">
-        <Image src={Sample} alt="medicineSample" width="150" height="300" />
-        <div className="flex flex-col gap-2 font-bold text-darkblue test-xl">
-          {/* 약 이름 */}
-          <div className="flex flex-row items-center justify-between w-full gap-2">
-            <span className="flex-[2]">이름</span>
-            <input
-              type="text"
-              placeholder="약 이름 입력"
-              className="w-full p-3 flex-[8]"
-            />
+    <div className="flex items-center justify-center w-[600px] h-[250px] bg-white border-4 rounded-2xl border-darkblue">
+      <div className="flex flex-col gap-3 font-bold text-darkblue text-xl">
+        {/* 약 이름 */}
+        <div className="flex flex-row items-center w-full gap-1">
+          <span className="w-[60px]">이름</span>
+          <input
+            type="text"
+            placeholder="약 이름 입력"
+          />
+        </div>
+        {/* <div className="flex flex-row items-center w-full gap-2">
+          <span className="w-[60px]">시간</span>
+          <div className="flex flex-row gap-7">
+            {time.map(time =>
+              <label key={time} htmlFor='select'>
+                <input
+                  type="checkbox"
+                  id="select"
+                  className="mr-2"
+                />
+                {time}
+               </label>
+            )}
           </div>
-          <div className="flex flex-row items-center justify-between w-full gap-2">
-            <span className="flex-[2]">시간</span>
-            <div className="flex flex-row gap-7">
-              <label className="flex flex-row gap-2">
-                <input
-                  type="checkbox"
-                  checked={isChecked}
-                  onChange={onChange}
-                />
-                <span>아침</span>
-              </label>
-              <label className="flex flex-row gap-2">
-                <input
-                  type="checkbox"
-                  checked={isChecked}
-                  onChange={onChange}
-                />
-                <span>점심</span>
-              </label>
-              <label className="flex flex-row gap-2">
-                <input
-                  type="checkbox"
-                  checked={isChecked}
-                  onChange={onChange}
-                />
-                <span>저녁</span>
-              </label>
+        </div> */}
+        <div className="flex flex-row items-center w-full gap-1">
+          <span className="w-[60px]">기타</span>
+          <input
+            type="text"
+            placeholder="추가 정보"
+          />
+        </div>
+        <div className="flex flex-row w-full gap-1">
+          <span className="w-[60px] pt-2">알람</span>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-row items-center justify-center text-lg">
+              <input
+                type="number"
+                value={hours}
+                onChange={handleHourSetting}
+                placeholder="시"
+                min="0" max="23"
+                className="border rounded p-2 w-[80px] h-[40px]"
+              />
+              <span className="px-4">:</span>
+              <input
+                type="number"
+                value={minutes}
+                onChange={handleMinuteSetting}
+                placeholder="분"
+                min="0" max="59"
+                className="border rounded p-2 w-[80px] h-[40px]"
+              />
             </div>
-          </div>
-          <div className="flex flex-row items-center justify-between w-full gap-1">
-            <span className="flex-[2]">이름</span>
+            <div className="flex flex-row gap-2">
+              {date.map(date =>
+                <label key={date} htmlFor="choose">
+                  <input
+                    type="checkbox"
+                    id="choose"
+                    className="mr-2"
+                  />
+                  {date}
+                </label>
+              )}
+            </div>
           </div>
         </div>
       </div>
