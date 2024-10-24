6ba86c8068063edc40466347becb46c70568463a
이현지 <dororong555@gmail.com>
2024-10-24 21:29:25 +0900

    manual

--- src/app/manual/page.tsx
+++ src/app/manual/page.tsx
@@ -20,7 +20,7 @@ const ManualPage: React.FC = () => {
           <div className="flex flex-row justify-center w-full flex-wrap mb-2">
             {manualItems.filter(item => item.category === '카카오톡').map(item => (
               <Link key={item.id} href={`/manual/${item.id}`}>
-                <div className="mt-2 mx-1 w-72 h-10 bg-gradient-to-r from-yellow-400 to-yellow-500 text-white rounded-lg text-lg flex items-center justify-center transition-transform duration-300 transform hover:scale-105 hover:bg-[#005bb5]">
+                <div className="mt-2 mx-1 w-72 h-10 bg-gradient-to-r from-yellow-400 to-yellow-500 dark:from-yellow-500 dark:to-yellow-600 text-white rounded-lg text-lg flex items-center justify-center transition-transform duration-300 transform hover:scale-105 hover:bg-[#005bb5]">
                   {item.title}
                 </div>
               </Link>
@@ -36,7 +36,7 @@ const ManualPage: React.FC = () => {
           <div className="flex flex-col items-center justify-center w-full">
             {manualItems.filter(item => item.category === '일상').map(item => (
               <Link key={item.id} href={`/manual/${item.id}`}>
-                <div className="mt-2 mx-1 w-72 h-10 bg-gradient-to-r from-green-400 to-green-500 text-white rounded-lg text-lg flex items-center justify-center transition-transform duration-300 transform hover:scale-105 hover:bg-[#005bb5] hover:text-white">
+                <div className="mt-2 mx-1 w-72 h-10 bg-gradient-to-r from-green-400 to-green-500 dark:from-green-500 dark:to-green-600 text-white rounded-lg text-lg flex items-center justify-center transition-transform duration-300 transform hover:scale-105 hover:bg-[#005bb5] hover:text-white">
                   {item.title}
                 </div>
               </Link>
